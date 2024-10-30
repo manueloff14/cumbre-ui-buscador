a2e163c67cc87ca2eaf7f54d92f8c09292fe2610
manueloff14 <manueloffcabrera@gmail.com>
2024-10-30 09:18:07 -0500

    prueba api en ENV

--- src/app/buscar/page.jsx
+++ src/app/buscar/page.jsx
@@ -23,7 +23,7 @@ export default async function Buscar({ searchParams }) {
   if (query) {
     try {
       const response = await fetch(
-        `https://api.cumbre.icu/buscar/${query}`
+        `${process.env.SEARCH_API_URL}/buscar/${query}`
       );
       if (!response.ok) {
         throw new Error("Network response was not ok");
