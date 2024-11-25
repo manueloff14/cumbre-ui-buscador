a2ea3875edbd4d6711d41bb01b0b7b63e22561a1
Manuel Cabrera <107600099+manueloff14@users.noreply.github.com>
2024-11-25 15:15:56 -0500

    Update Resultados.jsx

--- src/components/routes/buscar/Resultados.jsx
+++ src/components/routes/buscar/Resultados.jsx
@@ -22,6 +22,8 @@ export default function Resultados({ initialResults, error, query }) {
         return 'Computrabajo';
       case 'co.trabajosdiarios.com':
         return 'Trabajos Diarios';
+      case 'empleos.elpais.com.co':
+        return "Clasificados El País";
       default:
         return 'Fuente desconocida';
     }
@@ -35,6 +37,8 @@ export default function Resultados({ initialResults, error, query }) {
         return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFF3M9gOk6vZRAFrqt6dxJGhTS917I6_fdog&s";
       case 'co.trabajosdiarios.com':
         return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAP1BMVEUGR4Dx9fhag6lukbL///8APny9z94GR4AFR4AARH/f6PArW42huM2RrMXX4uyAoL1Ab5vI2OQVUoinv9Pl6/LwEGuFAAAACHRSTlOs////////1mqaPp0AAAC7SURBVCiRxdHdDoMgDAVgS6GjUv7U93/WVZ06pvNu2YmGiy+Qk7br/Nd0N6b6U+Q9J2R3oHPcIAfp91T03GC0b0mlQS8NmvamqQAwgx6ThL3Vgr4g5qQoiBjcMPB7Wy0ZSNG4gka/V+NtCLxinIT6UeLa+AMJMkGMlpaXT0gJqFpAd4GSY45IVq5uZoMGy2jjFQrmnPV10zw76JigEKxZ6+775DJWZExztuEfy9Zluflf0g7hMn/BG308Adg7F9ncwhKhAAAAAElFTkSuQmCC";
+      case 'empleos.elpais.com.co':
+        return "https://empleos.elpais.com.co/images/favicon.png";
       default:
         return null;
     }
