0cc8e6ed83b2ed274847dd90a32eb28635333dd2
manueloff14 <manueloffcabrera@gmail.com>
2025-01-02 11:30:23 -0500

    corregimos error de link del autocomplete desktop

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c12f8786c985e47"; }
+/******/ 		__webpack_require__.h = function() { return "fc24c505c762a07e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/DesktopAutocomplete.jsx
+++ src/components/DesktopAutocomplete.jsx
@@ -49,36 +49,35 @@ export function AutocompleteDesktop({
                             : description;
 
                     return (
-                        <li
-                            key={index}
-                            className="
+                        <li>
+                            <a href={`https://buscador.cumbre.icu/buscar?query=${encodeURIComponent(description)}`}
+                                key={index}
+                                className="
               px-3 py-2 flex items-center justify-between 
               hover:bg-gray-200 hover:text-black
               dark:hover:bg-gray-800 dark:hover:text-white
               cursor-pointer transition-colors
               rounded-xl
             "
-                            onClick={() => {
-                                router.push(`https://buscador.cumbre.icu/buscar?query=${encodeURIComponent(description)}`);
-                            }}
-                        >
-                            {/* Resaltar la coincidencia en negrita */}
-                            <span>
-                                <span className="text-gray-400 dark:text-gray-500">{matchText}</span>
-                                <span>{restText}</span>
-                            </span>
-                            <button
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    e.stopPropagation();
-                                    handleApplySuggestion(suggestion);
-                                }}
-                                className="p-1 rounded-full hover:bg-gray-400 dark:hover:bg-gray-700"
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
-                                    <path d="M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z" fill={isDarkMode ? "white" : "black"}></path>
-                                </svg>
-                            </button>
+                                {/* Resaltar la coincidencia en negrita */}
+                                <span>
+                                    <span className="text-gray-400 dark:text-gray-500">{matchText}</span>
+                                    <span>{restText}</span>
+                                </span>
+                                <button
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        e.stopPropagation();
+                                        handleApplySuggestion(suggestion);
+                                    }}
+                                    className="p-1 rounded-full hover:bg-gray-400 dark:hover:bg-gray-700"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 24 24">
+                                        <path d="M 19.171875 2 C 18.448125 2 17.724375 2.275625 17.171875 2.828125 L 16 4 L 20 8 L 21.171875 6.828125 C 22.275875 5.724125 22.275875 3.933125 21.171875 2.828125 C 20.619375 2.275625 19.895625 2 19.171875 2 z M 14.5 5.5 L 3 17 L 3 21 L 7 21 L 18.5 9.5 L 14.5 5.5 z" fill={isDarkMode ? "white" : "black"}></path>
+                                    </svg>
+                                </button>
+                            </a>
                         </li>
                     );
                 })}
