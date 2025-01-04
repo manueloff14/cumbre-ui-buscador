d6d2b304580ed667891c66094f9f91bcd7ee80f9
Manuel Cabrera <107600099+manueloff14@users.noreply.github.com>
2025-01-04 11:50:19 -0500

    activamos publicidad de monetag

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        {/* <script src="https://alwingulla.com/88/tag.min.js" data-zone="117033" async data-cfasync="false"></script> */}
+        <script src="https://alwingulla.com/88/tag.min.js" data-zone="117033" async data-cfasync="false"></script>
 
         {/* Google tag (gtag.js) */}
         <script
